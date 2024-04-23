a3c37d4cefac833403092336a8699b25ffc4cad4
manu-cj <carenjotje@gmail.com>
2024-04-23 16:19:24 +0200

    save

--- assets/js/component/main.js
+++ assets/js/component/main.js
@@ -1,20 +1,9 @@
-import restaurant from '../db/restaurant.js';
-
-const main = () => {
-    
-for (let i = 0; i < restaurant.length; i++) {
-    
-    
-}
-
-    const body = document.querySelector('body');
-    body.appendChild(document.createElement('div'));
-    let div = document.querySelectorAll('div')
 
 
+const mainComponent = () => {
 
 
 }
 
 //Pour importer dans app.js
-export default main;
+export default mainComponent;
