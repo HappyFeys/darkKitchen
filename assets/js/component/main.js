26cc6a0b8641c7c803cf6ea284ba633d1aaa6630
manu-cj <carenjotje@gmail.com>
2024-04-23 15:54:27 +0200

    save

--- assets/js/component/main.js
+++ assets/js/component/main.js
@@ -8,13 +8,24 @@ const mainComponent = () => {
     let main = document.querySelector('main');
     for (const type in produits) {
         if (produits.hasOwnProperty(type)) {
-            for (let i = 0; i < produits[type].length; i++) {
-                let produit = produits[type][i];
-                main.appendChild(document.createElement('article'));
-                let article = document.querySelectorAll("article");
-                article[i].innerHTML = `<h2>${produit.nom}</h2>`
+            produits[type].forEach(produit => {
+                //creation de l'html
+                let article = document.createElement('article');
+
+                //ajout de l'image dans la carte
+                let articleImage = document.createElement('div');
+                articleImage.style.backgroundImage = `url(${produit.image})`; 
+
+                //ajout du titre dans la carte
+                let articleTitle = document.createElement('h2');
+                articleTitle.textContent = produit.nom;
                 
-            }
+                
+                article.appendChild(articleImage);
+                article.appendChild(articleTitle);
+                
+                main.appendChild(article);
+            });
         }
     }
 
