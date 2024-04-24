0f5371b2dc11ef2a1e44b67a863d79acd1031eed
Justine-Frigo <justine0912@gmail.com>
2024-04-24 21:15:10 +0200

    added feature to close menu (oops)

--- assets/js/component/burgerMenu.js
+++ assets/js/component/burgerMenu.js
@@ -1,13 +1,18 @@
 const toggleBurgerMenu = () => {
-    const burgerMenu = document.getElementById('nav--btnHamburger');
+  const burgerMenu = document.getElementById("nav--btnHamburger");
 
-    const mobileMenu = document.getElementById('mobileMenu');
+  const mobileMenu = document.getElementById("mobileMenu");
 
-    burgerMenu.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('nav__mobileMenu');
-    })
+  burgerMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("nav__mobileMenu");
+  });
 
-}
+  const closeMenu = document.getElementById("closeMenu");
+  closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("nav__mobileMenu");
+  });
+};
 
 export default toggleBurgerMenu;
