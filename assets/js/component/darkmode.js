3772d6e765d03ab0fbf05a9ecac740f3f66a8b37
Mohamed <mohamed.md5577@gmail.com>
2024-04-25 10:24:35 +0200

    dark mode panier

--- assets/js/component/darkmode.js
+++ assets/js/component/darkmode.js
@@ -10,7 +10,7 @@ const switchMode = () => {
 
     if (document.body.classList.contains('light-mode')) {
         mainCssFile.href = "./assets/css/darkmodeMain.css";
-        cartCssFile.href = "./assets/css/darkmodeCart.css"
+        cartCssFile.href = "./assets/css/darkmodePanier.css"
         document.body.classList.add('dark-mode');
         document.body.classList.remove('light-mode');
     } else if (document.body.classList.contains('dark-mode')) {
